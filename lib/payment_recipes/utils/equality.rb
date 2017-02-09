8d08a2c64d28716bf0ed121ab6e75e9957697831
Joshua Arvin Lat <joshua.arvin.lat@gmail.com>
2017-02-09 19:49:37 +0800

    Fix equality and bump version

--- lib/payment_recipes/utils/equality.rb
+++ lib/payment_recipes/utils/equality.rb
@@ -2,7 +2,7 @@ module PaymentRecipes
   module Utils
     module Equality
       def ==(other)
-        @id = other.id && self.class.name == other.class.name
+        @id == other.id && self.class.name == other.class.name
       end
     end
   end
